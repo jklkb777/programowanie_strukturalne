78c2a06f8c85d4ed06ba9617c4a03d48ea28d623
jklkb777 <77102058+jklkb777@users.noreply.github.com>
2021-02-25 16:44:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+c = 2
+
+dodawanie = a + b
+mnożenie = a * c
+dzielenie = a/b
+dzielenie_calkowite = b // c
+potęga = c ** b
+
+print(dodawanie, mnożenie)
+print(mnożenie)
+print(dzielenie)
+print(dzielenie_calkowite)
+print(potęga)
+print(pow(8, 2))
+a += c
+print(a)
