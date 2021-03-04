81b84c8feab2243115c7b313d2cebfdcce3d65d9
jklkb777 <77102058+jklkb777@users.noreply.github.com>
2021-03-04 10:18:34 +0100

    pd

--- lab1.py
+++ lab1.py
@@ -1,36 +1,101 @@
-#zad2
-user_choice = 0
+from math import *
 
-def dodawanie(x, y):
-    return x + y
+#zad1
+# int1 = 1
+# int2 = 2
+# float1 = 4.2
+# float2 = 3.6
+# string1 = "Tu jest ciąg znaków"
+# string2 = "Ala ma kota"
+# bool1 = 0
+# bool2 = 1
+#
+# print(int1, int2, float1, float2, string1, string2, bool1, bool2)
 
-def odejmowanie(x, y):
-    return x - y
+# zad2
+# a = input("Wprowadź a ")
+# b = input("Wprowadź b ")
+#
+# dodawanie = a + b
+# odejmowanie = a - b
+# mnozenie = a * b
+# dzielenie = a/b
+#
+# print(dodawanie,odejmowanie,mnozenie,dzielenie)
+#
+#zad3
 
-def mnożenie(x, y):
-    return x * y
+# a = 7
+# b = 3
+#
+# a += b
+#
+# a -= b
+#
+# a *= b
+#
+# a /= b
+#
+# a %= b
 
-def dzielenie(x, y):
-    return x / y
+#zad 4
+print(exp(10))
+
+sinus = pow(sin(8), 2)
+logarithm = 5 + sinus
+dzialanie = pow(log(logarithm, e), 1/6)
+print(dzialanie)
+
+x = 3.55
+y = 4.80
+print(ceil(x))
+print(ceil(y))
 
 
-while user_choice != 5:
-    if user_choice == 1:
-        dodawanie()
-    elif user_choice == 2:
-        odejmowanie()
-    elif user_choice == 3:
-        mnożenie()
-    elif user_choice == 4:
-        dzielenie()
-    elif user_choice == 5:
-        break
+#zad5
+# imie = input("Wpisz imię: ")
+# nazwisko = input("Wpisz nazwisko: ")
+#
+# print(imie.capitalize() + " " + nazwisko.capitalize())
 
-    print()
-    print("1. Dodawanie")
-    print("2. Odejmowanie")
-    print("3. Mnożenie")
-    print("4. Dzielenie")
-    print("5. Koniec")
+#zad6
+# txt = """Zaliczona matura na pięć
+# Są tacy - to nie żart
+# Dla których jesteś wart
+# Mniej niż zero
+# Mniej niż zero
+# Mniej niż zero
+# Mniej niż zero
+# Oh oh oh oh"""
+# print(txt)
+# x = txt.count("zero")
+# print("W tekście słowo 'zero' padło: ")
+# print(x)
 
-    user_choice = input("Wybierz liczbę: ")
+#zad7
+# txt = "Wiosna jest piękna"
+# print(txt[1])
+# print(txt[-1])
+
+#zad8
+# txt = """Zaliczona matura na pięć
+# Są tacy - to nie żart
+# Dla których jesteś wart
+# Mniej niż zero
+# Mniej niż zero
+# Mniej niż zero
+# Mniej niż zero
+# Oh oh oh oh"""
+#
+#
+# splitted_txt = txt.split()
+#
+# print(splitted_txt)
+
+#zad 9
+
+string = "tekst"
+float = 3.14
+hex_number = 666
+
+print("%s %f %s" % (string, float, hex(hex_number)))
